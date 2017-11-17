f047bd09e137b1e07d1ac95b505c8f2df20a1580
Matthew Randall <matrandall@gmail.com>
2017-11-18 00:29:55 +1300

    Added guns and a target

---
+++ Assets/Characters/Player/Scripts/GunController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+	private GameObject _rightGun;
+	private GameObject _leftGun;
+
+	Shooter _rightIShoot;
+    Shooter _leftIShoot;
+
+	// Use this for initialization
+	void Start () {
+		_rightGun = GameObject.FindGameObjectWithTag ("RightGun");
+		_rightIShoot = _rightGun.GetComponent<Shooter> ();
+
+		_leftGun = GameObject.FindGameObjectWithTag ("LeftGun");
+		_leftIShoot = _leftGun.GetComponent<Shooter> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetButton ("Fire1")) {
+			_rightIShoot.Fire ();
+		}
+
+		if (Input.GetButton ("Fire2")) {
+			_leftIShoot.Fire ();
+		}
+	}
+}
---
+++ Assets/Characters/Scripts/Shootable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shootable : MonoBehaviour {
+
+    public float HitPoints = 30;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public void Damage(RaycastHit hit, float damagePerHit, float hitForce)
+    {
+        HitPoints -= damagePerHit;
+
+        if (hit.rigidbody != null)
+        {
+            hit.rigidbody.AddForce(-hit.normal * hitForce);
+        }
+    }
+}
